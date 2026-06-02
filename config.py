7932fc2b16e42cd935c273d3e47a7c8df7902c46
Rustam <kodzokoff2016@yandex.ru>
2026-06-02 15:12:39 +0300

    deleted comments

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из файла .env в окружение
 load_dotenv()
+
 
 class Config:
     BASE_URL = os.getenv("QIWI_BASE_URL", "https://qiwi.com")
@@ -10,6 +10,5 @@ class Config:
     POINT_ID = os.getenv("QIWI_POINT_ID")
     TOKEN = os.getenv("QIWI_TOKEN")
 
-    # Простая проверка, что критически важные данные были загружены
     if not all([AGENT_ID, POINT_ID, TOKEN]):
         raise ValueError("Ошибка: Проверьте, что QIWI_AGENT_ID, QIWI_POINT_ID и QIWI_TOKEN заполнены в файле .env")
